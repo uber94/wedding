9469dfe3da5dc7a4c72d67e0896af57fecefc533
H.AN <hubertnang2017@gmail.com>
2023-01-06 23:11:09 +0100

    roll

--- js/scripts.js
+++ js/scripts.js
@@ -145,21 +145,21 @@ $(document).ready(function () {
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(po, s);
 
-    // for (var i = 0; i < share_bar.length; i++) {
-    //     var html = '<iframe allowtransparency="true" frameborder="0" scrolling="no"' +
-    //         'src="https://platform.twitter.com/widgets/tweet_button.html?url=' + encodeURIComponent(window.location) + '&amp;text=' + encodeURIComponent(document.title) + '&amp;via=ramswarooppatra&amp;hashtags=ramandantara&amp;count=horizontal"' +
-    //         'style="width:105px; height:21px;">' +
-    //         '</iframe>' +
+    for (var i = 0; i < share_bar.length; i++) {
+        var html = '<iframe allowtransparency="true" frameborder="0" scrolling="no"' +
+            'src="https://platform.twitter.com/widgets/tweet_button.html?url=' + encodeURIComponent(window.location) + '&amp;text=' + encodeURIComponent(document.title) + '&amp;via=ramswarooppatra&amp;hashtags=ramandantara&amp;count=horizontal"' +
+            'style="width:105px; height:21px;">' +
+            '</iframe>' +
 
-    //         '<iframe src="//www.facebook.com/plugins/like.php?href=' + encodeURIComponent(window.location) + '&amp;width&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;share=true&amp;height=21&amp;appId=101094500229731&amp;width=150" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:150px; height:21px;" allowTransparency="true"></iframe>' +
+            '<iframe src="//www.facebook.com/plugins/like.php?href=' + encodeURIComponent(window.location) + '&amp;width&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;share=true&amp;height=21&amp;appId=101094500229731&amp;width=150" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:150px; height:21px;" allowTransparency="true"></iframe>' +
 
-    //         '<div class="g-plusone" data-size="medium"></div>';
+            '<div class="g-plusone" data-size="medium"></div>';
 
-    //     // '<iframe src="https://plusone.google.com/_/+1/fastbutton?bsv&amp;size=medium&amp;url=' + encodeURIComponent(window.location) + '" allowtransparency="true" frameborder="0" scrolling="no" title="+1" style="width:105px; height:21px;"></iframe>';
+        // '<iframe src="https://plusone.google.com/_/+1/fastbutton?bsv&amp;size=medium&amp;url=' + encodeURIComponent(window.location) + '" allowtransparency="true" frameborder="0" scrolling="no" title="+1" style="width:105px; height:21px;"></iframe>';
 
-    //     share_bar[i].innerHTML = html;
-    //     share_bar[i].style.display = 'inline-block';
-    // }
+        share_bar[i].innerHTML = html;
+        share_bar[i].style.display = 'inline-block';
+    }
 
     /********************** Embed youtube video *********************/
     $('.player').YTPlayer();
@@ -184,10 +184,10 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Mariage de Xaverie et Hubert",
+            title: "Ram and Antara's Wedding",
 
             // Event start date
-            start: new Date('Aug 19, 2023 10:00'),
+            start: new Date('Nov 27, 2017 10:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
